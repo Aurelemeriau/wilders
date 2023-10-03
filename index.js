40c863d06d1bf89ac3866f932ae5f48744cf2653
Aurelien <aurelien.emeriau@hotmail.fr>
2023-10-03 10:43:47 +0200

    ajout fichiers

---
+++ index.js
@@ -0,0 +1,21 @@
+
+import chalk from "chalk"
+
+console.log(
+
+    chalk.blue("Guillaume")
+)
+
+console.log(
+    chalk.red("Hélène")
+)
+
+console.log(
+    chalk.yellow("Irvin")
+)
+
+console.log(
+    chalk.green("Emmanuelle")
+)
+
+
